1452653ad86467fc23cb0ac483e077abcb20645f
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2021-05-17 14:54:58 -0300

    Adiciona erro.js e experimentos.js

---
+++ fundamentos/erro.js
@@ -0,0 +1,24 @@
+function tratarErroELancar(error) {
+    throw new Error ('Ocorreu um erro no processamento, nossa equipe de suporte já foi avisada')
+    throw 10
+    throw true
+    throw 'mensagem'
+    throw {
+        nome: error.name,
+        msg: error.message,
+        date: new Date
+    }
+}
+
+function imprimirNomeGritado(obj) {
+    try{
+        console.log(obj.nome.toUpperCase() + '!!!')
+    } catch(e) {
+        tratarErroELancar(e)
+    } finally {
+        console.log('Final')
+    }
+}
+
+const obj = { nome: 'Roberto' }
+imprimirNomeGritado(obj)
