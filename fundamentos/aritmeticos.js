c62d8a174dc16610ae14fc400595e8dc37e2d950
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2021-05-17 01:22:15 -0300

    Adiciona arquivo aritmeticos.js

---
+++ fundamentos/aritmeticos.js
@@ -0,0 +1,12 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d // são operações binárias, envolvem 2 operadores (a + b) e sintaxe do tipo in fix
+// a + b in fix
+// a++ pos fix
+// ++a pre fix
+const subtracao =  d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2 // resto da divisão
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
