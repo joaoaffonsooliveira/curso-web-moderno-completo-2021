ca10d77074b7a4cc69a8054b1ef2f969c923325c
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2021-05-18 09:12:39 -0300

    Adiciona while.js

---
+++ controle/while.js
@@ -0,0 +1,16 @@
+// estrutura de repetição while.
+// O while é usando quando quero repetir algo um número de vezes indeterminado até que uma condição true or false seja cumprida
+
+function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let opcao = 0
+
+while(opcao != -1) {
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log(`Opção escolhida foi ${opcao}.`)
+}
+
+console.log('Até a próxima')
