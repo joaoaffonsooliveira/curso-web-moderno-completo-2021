87e48458550a94ef1ef1a493dd545375550d29f8
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2021-05-20 11:33:18 -0300

    Atualiza callApply.js

--- funcao/callApply.js
+++ funcao/callApply.js
@@ -1,3 +1,5 @@
+// Call e Apply são outras formas de chamar uma função. Call e Aplly são funções também
+
 function getPreco(imposto = 0, moeda = 'R$') {
     return `${moeda} ${this.preco * (1 - this.desc) * (1 + imposto)}`
 }
@@ -11,13 +13,14 @@ const produto = {
 
 global.preco = 20
 global.desc = 0.1
-console.log(getPreco())
-console.log(produto.getPreco())
+console.log(getPreco()) // Aqui chamamos a função diretamente
+console.log(produto.getPreco()) // Aqui chamamos a função por meio de um objeto
 
 const carro = { preco: 49990, desc: 0.20 }
 
-console.log(getPreco.call(carro))
-console.log(getPreco.apply(carro))
+console.log(getPreco.call(carro)) // Aqui chamamos a função pelo call
+console.log(getPreco.apply(carro)) // Aqui chamamos a função pelo apply
 
-console.log(getPreco.call(carro, 0.17, '$'))
-console.log(getPreco.apply(global, [0.17, '$']))
+// A diferença entre o call e o apply está na forma como passamos os parâmetros
+console.log(getPreco.call(carro, 0.17, '$')) 
+console.log(getPreco.apply(global, [0.17, '$'])) // na apply os parâmetros são passados por meio de um array
