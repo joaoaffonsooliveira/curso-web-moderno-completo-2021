0b08ac9211fb0e7a00c8a04f91293a51df4a5ea5
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2021-05-19 23:43:03 -0300

    Atualiza tiposDeclaracao.js

--- funcao/tiposDeclaracao.js
+++ funcao/tiposDeclaracao.js
@@ -1,3 +1,6 @@
+/* Mesmo na linha um já consigo chamar uma função do tipo "function declaration"
+porque o javascript carrega primeiro todas as funções desse tipo. O mesmo não ocorre com a "function
+expression" e nem com a "named function expression". Ambas precisam ser chamadas após sua declaração */
 console.log(soma(3, 4))
 
 // function declaration
@@ -9,10 +12,10 @@ function soma(x, y) {
 const sub = function (x, y) {
     return x - y
 }
-console.log(sub(3, 4))
+console.log(sub(3, 4)) // function expression chamada depois de declarada
 
 // named function expression
 const mult = function mult(x, y) {
     return x * y
 }
-console.log(mult(3, 4))
+console.log(mult(3, 4)) // named function expression chamada depois de declarada
