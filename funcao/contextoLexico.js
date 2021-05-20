6df260685a45f97f3325a968d969e4ad96b9d831
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2021-05-19 23:49:32 -0300

    Atualiza contextoLexico.js

--- funcao/contextoLexico.js
+++ funcao/contextoLexico.js
@@ -1,3 +1,6 @@
+/* A função carrega consigo a informação do local aonde ela foi criada, ou seja o contexto léxico
+de quando foi criada */
+
 const valor = 'Global'
 
 function minhaFuncao() {
@@ -6,7 +9,7 @@ function minhaFuncao() {
 
 function exec() {
     const valor = 'Local'
-    minhaFuncao()
+    minhaFuncao() // chama a função de cima que tem valor setado como 'Global' naquele contexto léxico
 }
 
 exec()
