547552c2c6ddfe9b2c83f317bdc0a5cc4a04eb95
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2021-05-24 19:20:42 -0300

    Atualiza jsonVsObj.js

--- objeto/jsonVsObj.js
+++ objeto/jsonVsObj.js
@@ -1,7 +1,16 @@
-const obj = { a: 1, b: 2, c: 3, soma() { return a + b + c } }
-console.log(JSON.stringify(obj))
+/* O JSON é o mais utilizado no mercado para fazer a comunicação entre sistemas de tecnologias
+totalmente diferentes. É um formato textual que consegue ser lido em qualquer linguagem praticamente */
 
-// console.log(JSON.parse("{ a: 1, b: 2, c: 3 }"))
-// console.log(JSON.parse("{ 'a': 1, 'b': 2, 'c': 3 }"))
-console.log(JSON.parse('{ "a": 1, "b": 2, "c": 3 }'))
-console.log(JSON.parse('{ "a": 1.7, "b": "string", "c": true, "d": {}, "e": [] }'))
+const obj = { a: 1, b: 2, c: 3, soma() { return a + b + c } }
+console.log(JSON.stringify(obj)) /* No JSON não conseguimos passar funções, apenas dados de outros tipos
+porque o JSON é um arquivo de texto, feito essencialmente para passagem de dados entre sistemas feitos com
+tecnologias diferentes */
+
+// console.log(JSON.parse("{ a: 1, b: 2, c: 3 }")) --> o formato JSON não é esse
+// console.log(JSON.parse("{ 'a': 1, 'b': 2, 'c': 3 }")) --> o formato JSON não é esse
+console.log(JSON.parse('{ "a": 1, "b": 2, "c": 3 }')) /* Agora sim escrevemos um JSON. todos os atributos de um JSON
+precisam estar entre aspas duplas. A função JSON.parse transforma o JSON em objeto. */
+console.log(JSON.parse('{ "a": 1.7, "b": "string", "c": true, "d": {}, "e": [] }')) /* Aqui também temos um JSON
+e recebe todos os tipos uma string precisa ter aspas duplas. Uma stringo como no exemplo abaixo não funcionaria */
+
+// console.log(JSON.parse('{ "a": 1.7, "b": \'string\', "c": true, "d": {}, "e": [] }'))
