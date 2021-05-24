88b1dbe97fe0ec2f2b198b840f44a516a659b741
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2021-05-24 18:36:18 -0300

    Atualiza thisEBind2.js

--- funcao/thisEBind2.js
+++ funcao/thisEBind2.js
@@ -6,10 +6,12 @@ function Pessoa() {
     setInterval(function() {
         this.idade++
         console.log(this.idade)
-    }.bind(this), 1000)
+    }.bind(this), 1000) // a cada 1 segundo a função anônima é disparada
 }
+/*Sem o .bind(this) no final geraria um erro porque o this não estaria apontando para a pessoa criada abaixo */
 
-new Pessoa
+new Pessoa // instanciando um objeto
+
 
 // Segunda forma de driblar a variação do this é com o artifício tecnologico de uma variável.
 
@@ -22,5 +24,7 @@ function Pessoa() {
         console.log(self.idade)
     }, 1000)
 }
+/* Como self é uma constante, ele se mantém ao longo do código, justamente por isso não importa quem chama a função
+o self sempre apontará p/ pessoa porque o self não varia, é uma forma de travar o valor de this para o objeto.*/
 
 new Pessoa
