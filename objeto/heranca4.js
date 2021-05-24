883fd94c1cf0240248bf35d4a1ebd763ef78da89
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2021-05-24 01:24:03 -0300

    Atualiza heranca4.js

--- objeto/heranca4.js
+++ objeto/heranca4.js
@@ -1,17 +1,20 @@
 function MeuObjeto() {}
 console.log(MeuObjeto.prototype)
 
-const obj1 = new MeuObjeto
-const obj2 = new MeuObjeto
-console.log(obj1.__proto__ === obj2.__proto__)
-console.log(MeuObjeto.prototype === obj1.__proto__)
+const obj1 = new MeuObjeto // instanciei objeto a partir de uma função construtora
+const obj2 = new MeuObjeto // instanciei outro objeto a partir da mesma função construtora
+console.log(obj1.__proto__ === obj2.__proto__) /* true porque os dois objetos apontam para o mesmo protótipo
+que nesse caso foi a função construtora */
+console.log(MeuObjeto.prototype === obj1.__proto__) /* true. o protótipo do meu objeto aponta para o protótipo
+da função construtora, que não é o object.prototype e sim MeuObjeto.prototype, ou seja o
+atributo prototype da função construtora */
 
 MeuObjeto.prototype.nome = 'Anônimo'
 MeuObjeto.prototype.falar = function() {
     console.log(`Bom dia! Meu nome é ${this.nome}!`)
 }
 
-obj1.falar()
+obj1.falar() /* Obj1 recebe nome do protótipo pai porque adicionamos nome nos códigos acima*/
 
 obj2.nome = 'Rafael'
 obj2.falar()
@@ -25,4 +28,4 @@ obj3.falar()
 console.log((new MeuObjeto).__proto__ === MeuObjeto.prototype)
 console.log(MeuObjeto.__proto__ === Function.prototype)
 console.log(Function.prototype.__proto__ === Object.prototype)
-console.log(Object.prototype.__proto__ === null)
+console.log(Object.prototype.__proto__ === null) // nulo porque o object.prototype é o nível máximo
