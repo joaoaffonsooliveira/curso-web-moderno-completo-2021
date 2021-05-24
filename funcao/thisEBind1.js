5387899f6e7a683d11946bc0705fb35e6719c8e7
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2021-05-24 18:26:35 -0300

    Atualiza thisEBind1.js

--- funcao/thisEBind1.js
+++ funcao/thisEBind1.js
@@ -1,3 +1,5 @@
+// O this da função arrow ele é um this associado ao contexto léxico
+
 const pessoa = {
     saudacao: 'Bom dia!',
     falar() {
@@ -11,3 +13,7 @@ falar() // conflito entre paradigmas: funcional e OO
 
 const falarDePessoa = pessoa.falar.bind(pessoa)
 falarDePessoa()
+
+const falar2 = pessoa.falar /*pessoa.falar não foi alterado pelo bind. pessoa.falar.bind(pessoa) 
+foi armazenado em falarDePessoa no caso anterior*/
+falar2() // retorna undefined
