0e30a7f2883214c15a632dec573155848cca27a3
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2021-05-25 12:21:36 -0300

    Atualiza filter1.js e reduce1.js

--- array/filter1.js
+++ array/filter1.js
@@ -1,3 +1,5 @@
+/* Quando quiser criar um novo array com os valores mudados usar o map. 
+Quando quiser apenas filtrar usar o filter*/
 const produtos = [
     { nome: 'Notebook', preco: 2499, fragil: true },
     { nome: 'iPad Pro', preco: 4199, fragil: true },
@@ -6,10 +8,17 @@ const produtos = [
 ]
 
 console.log(produtos.filter(function(p) {
-    return false
+    return false // todos os elementos são filtrados, nenhum deles passa, logo retorna []
 }))
 
 const caro = produto => produto.preco >= 500
 const fragil = produto => produto.fragil
 
-console.log(produtos.filter(caro).filter(fragil))
+console.log(produtos.filter(caro).filter(fragil)) 
+/* cria um array, que aparece somente preços acima de 500
+e ao mesmo tempo que tenha fragil como true */
+
+/* 
+{ nome: 'Notebook', preco: 2499, fragil: true }
+{ nome: 'iPad Pro', preco: 4199, fragil: true } 
+*/
--- array/reduce1.js
+++ array/reduce1.js
@@ -5,10 +5,10 @@ const alunos = [
     { nome: 'Ana', nota: 8.7, bolsista: true }
 ]
 
-console.log(alunos.map(a => a.nota))
+console.log(alunos.map(a => a.nota)) // [ 7.3, 9.2, 9.8, 8.7 ]
 const resultado = alunos.map(a => a.nota).reduce(function(acumulador, atual) {
-    console.log(acumulador, atual)
+    console.log(acumulador, atual) // para ver o passo a passo, como funciona
     return acumulador + atual
 }, 0)
 
-console.log(resultado)
+console.log(resultado) // 35
