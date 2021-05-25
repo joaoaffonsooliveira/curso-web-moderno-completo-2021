8001178c348bfe5c0b2b08df5127e8836543e334
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2021-05-25 11:50:23 -0300

    Atualiza map3.js

--- array/map3.js
+++ array/map3.js
@@ -1,6 +1,7 @@
+// Criando minha função map
 Array.prototype.map2 = function(callback) {
     const newArray = []
-    for (let i = 0; i < this.length; i++) {
+    for(let i = 0; i < this.length; i++) {
         newArray.push(callback(this[i], i, this))
     }
     return newArray
