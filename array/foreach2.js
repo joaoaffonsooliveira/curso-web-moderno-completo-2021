bdce6c9460746a3af96d87bdeece8740aba83f2a
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2021-05-25 10:56:29 -0300

    Atualiza foreach2.js

--- array/foreach2.js
+++ array/foreach2.js
@@ -1,11 +1,12 @@
-Array.prototype.forEach2 = function(callback) {
-    for (let i = 0; i < this.length; i++) {
+Array.prototype.forEach2 = function (callback) {
+    for(let i = 0; i < this.length; i++) { // acesso a instância de um array pelo this
         callback(this[i], i, this)
     }
 }
 
 const aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
 
+// gerava erro porque não tinha forEach2 
 aprovados.forEach2(function(nome, indice) {
-    console.log(`${indice + 1}) ${nome}`)
+    console.log(`${indice + 1} ${nome}`)
 })
