09df730be9f6054bd8c8763a3dd1fa00de8a0856
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2021-05-27 23:38:05 -0300

    Aprendendo sistema de módulos

--- node/exportar.js
+++ node/exportar.js
@@ -1,18 +1,19 @@
-console.log(module.exports)
-console.log(module.exports === this)
-console.log(module.exports === exports)
+console.log(module.exports) // {}
+console.log(module.exports === this) // true
+console.log(module.exports === exports) // true
 
 this.a = 1
 exports.b = 2
 module.exports.c = 3
 
+// não importa atribuir valores p/ exports porque o que é exportado é module.exports
 exports = null
-console.log(module.exports)
+console.log(module.exports) // { a: 1, b: 2, c: 3 }
 
+// não importa atribuir valores p/ exports porque o que é exportado é module.exports
 exports = {
     nome: 'Teste'
 }
+console.log(module.exports) // { a: 1, b: 2, c: 3 }
 
-console.log(module.exports)
-
-module.exports = { publico: true }
+module.exports = { publico: true}
--- node/exportarCliente.js
+++ node/exportarCliente.js
@@ -1,2 +1,6 @@
 const exportar = require('./exportar')
+// const exportar = require('./ExPoRtar') // cuidado que no linux vai dar errado! use sempre o mesmo nome do arquivo
+// const exporar = require('C:\workspace\curso-web-moderno-completo-2021\node\exportar.js') // dá certo, mas não é recomendado
+
 console.log(exportar)
+// só foi importado de exportar.js os que tinham module.exports
