272efbd2de7deab6505d9dd5785a35f3171d3d95
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2021-05-28 00:53:35 -0300

    Aprendendo a passar parâmetros module.exports e require

--- node/passandoParametrosCliente.js
+++ node/passandoParametrosCliente.js
@@ -1,2 +1,3 @@
+// Passando parâmetros usando o require de outro arquivo
 const saudacoes = require('./passandoParametros')('Ana', 'Lucas', 'João')
-console.log(saudacoes)
+console.log(saudacoes) // [ 'Boa semana Ana!', 'Boa semana Lucas!', 'Boa semana João!' ]
