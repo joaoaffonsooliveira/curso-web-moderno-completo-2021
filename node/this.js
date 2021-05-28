8f8093ee336fd35c09571666d60925fcf4a86db2
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2021-05-28 00:49:45 -0300

    Aprendendo this no node

--- node/this.js
+++ node/this.js
@@ -1,14 +1,21 @@
-console.log(this === global)
-console.log(this === module)
+//no node o this é um objeto que aponta para module.exports
+console.log(this === global) // false
+//no node o this é um objeto que aponta para module.exports
+console.log(this === module) // false
 
-console.log(this === module.exports)
-console.log(this === exports)
+console.log(this === module.exports) // true
+//o exports aponta para a mesma referência que é o module.exports
+console.log(this === exports) // true
 
 function logThis() {
     console.log('Dentro de uma função...')
-    console.log(this === exports)
-    console.log(this === module.exports)
-    console.log(this === global)
+    //dentro de uma função o this não aponta para exports
+    console.log(this === exports) // false
+    //dentro de uma função o this não aponta para exports
+    console.log(this === module.exports) // false
+    //dentro de uma função o this aponta para global e fora da função, ou seja, dentro do module aponta para module.exports
+    console.log(this === global) // true
+    // tenha cuidado com isso. thi.perigo = '...' por exemplo, aponta para global
 }
 
 logThis()
