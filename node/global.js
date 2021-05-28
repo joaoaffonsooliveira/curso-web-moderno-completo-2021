0c1e8c3dfebc458c05aa77418d73d5e5f3eec9b3
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2021-05-28 00:35:54 -0300

    Aprendendo a utilizar o global no node

--- node/global.js
+++ node/global.js
@@ -5,3 +5,7 @@ global.MinhaApp = Object.freeze({
     },
     nome: 'Sistema Legal'
 })
+
+// Pode ser muito perigoso ter um atributo que pode ser acessado de forma global. Isso dá muita margem para erros
+// esse é um dos motivos para termos o node porque dividimos tudo deixando as coisas mais organizadas.
+// Se eventualmente precisar usar um atributo de forma global use o Object.freeze para evitar possíveis bugs
--- node/globalCliente.js
+++ node/globalCliente.js
@@ -1,6 +1,6 @@
 require('./global')
 
-console.log(MinhaApp.saudacao())
+console.log(MinhaApp.saudacao()) // Estou em todos os lugares
 
-MinhaApp.nome = 'Eita!'
-console.log(MinhaApp.nome)
+MinhaApp.nome = 'Eita!' // Como usamos o object.freeze não conseguimos fazer essa mudança
+console.log(MinhaApp.nome) // Sistema legal
